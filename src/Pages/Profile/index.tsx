fa09560721967ab7969241565c20f0b978bd7cfc
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-08-03 13:44:56 -0300

    FEAT: fetching user photo

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,3 +1,64 @@
+import { ArrowLeft } from "phosphor-react";
+import { Container, IconButton, Nav, NavButton, PhotoDiv, ProfileHeader, TitleDiv } from "./styles";
+
+import { defaultTheme } from "../../styles/theme/default";
+import userThumb from '../../assets/profile-user.png'
+
+import { useNavigate } from "react-router-dom";
+import { BASE_URL, api } from "../../services/api";
+import { useEffect, useState } from "react";
+
 export function Profile() {
-    return <h1>Profile page works!</h1>
+    const navigate = useNavigate()
+    const userLocalStorage = localStorage.getItem("@aluchef:user")
+    const user = userLocalStorage ? JSON.parse(userLocalStorage) : {}
+
+    const [userPhoto, setUserPhoto] = useState('')
+    
+    function handleGoBack() {
+        navigate(-1)
+    }
+    
+
+    async function fetchUser() {
+        const response = await api.get(`/api/users/${user.id}?populate=*`)
+
+        return response.data
+    }
+
+    useEffect(() => {
+        async function fetchUserPhoto() {
+            const response = await fetchUser()
+
+            setUserPhoto(response.photo.url)
+        }
+
+        fetchUserPhoto()
+
+    }, [user])
+
+    return <Container>
+        <ProfileHeader>
+            <PhotoDiv>
+                <img src={userPhoto ? `${BASE_URL}${userPhoto}` : userThumb } alt="" />
+                <span>{user.username}</span>
+            </PhotoDiv>
+
+            <IconButton onClick={handleGoBack}>
+            <ArrowLeft size={24} color={defaultTheme.COLORS.BLUE_300} />
+            </IconButton>
+        </ProfileHeader>
+
+        <TitleDiv>
+            <h2>Meu livro de Receitas</h2>
+        </TitleDiv>
+        <Nav>
+            <NavButton>
+                Criar Receita
+            </NavButton>
+            <NavButton>
+                Lista de compras
+            </NavButton>
+        </Nav>
+    </Container>
 }
---
+++ src/Pages/Profile/styles.ts
@@ -0,0 +1,103 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ProfileHeader = styled.header`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 4rem;
+  padding: 0 2.5rem;
+`;
+
+export const PhotoDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 1rem;
+
+  > img {
+    width: 8rem;
+    height: 8rem;
+
+    border-radius: 100%;
+  }
+
+  > span {
+    color: ${({ theme }) => theme.COLORS.BLUE_300};
+    text-align: center;
+
+    font-size: 1.5rem;
+    font-style: normal;
+    font-weight: 400;
+  }
+`;
+
+export const IconButton = styled.button`
+  all: unset;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid ${({ theme }) => theme.COLORS.BLUE_300};
+  border-radius: 100%;
+
+  padding: 0.5rem;
+
+  cursor: pointer;
+`;
+
+export const TitleDiv = styled.div`
+  width: 100%;
+
+  text-align: center;
+
+  background-color: ${({ theme }) => theme.COLORS.YELLOW};
+
+  padding: 1rem;
+
+  margin-top: 3rem;
+
+  > h2 {
+    color: ${({ theme }) => theme.COLORS.BLUE_300};
+
+    font-size: 1.8rem;
+    font-weight: 900;
+  }
+`;
+
+export const Nav = styled.nav`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  background-color: ${({ theme }) => theme.COLORS.PINK_300};
+  padding: 1rem;
+`;
+
+export const NavButton = styled.button`
+  all: unset;
+
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+
+  color: ${({ theme }) => theme.COLORS.BLUE_300};
+
+  font-size: 1.8rem;
+  font-weight: 900;
+
+  border-radius: 16px;
+
+padding: 1rem;
+`;
